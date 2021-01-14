e0b3bc53a54e1534317db72516021147499e77cc
DavidRossBergerson <73494751+DavidRossBergerson@users.noreply.github.com>
2021-01-14 18:04:19 -0500

    evened out solve ui

--- js/site.js
+++ js/site.js
@@ -1,54 +1,59 @@
-document.getElementById("btnActivate").addEventListener("click", SunsetHills)
-document.getElementById("btnClear").addEventListener("click", Clear)
+    document.getElementById("btnActivate").addEventListener("click", SunsetHills)
+    document.getElementById("btnClear").addEventListener("click", Clear)
 
-
-
-
-// buttons hear, make a function for clear button
-
-
-function SunsetHills(){
+    function SunsetHills(){
     // Step 1: Get all the user data
-    let bldg1 = parseInt(document.getElementById("input1").value);
-    let bldg2 = parseInt(document.getElementById("input2").value);
-    let bldg3 = parseInt(document.getElementById("input3").value);
-    let bldg4 = parseInt(document.getElementById("input4").value);
-    let bldg5 = parseInt(document.getElementById("iInput5").value);
+        let bldg1 = parseInt(document.getElementById("input1").value);
+        let bldg2 = parseInt(document.getElementById("input2").value);
+        let bldg3 = parseInt(document.getElementById("input3").value);
+        let bldg4 = parseInt(document.getElementById("input4").value);
+        let bldg5 = parseInt(document.getElementById("input5").value);
 
-    // step 2: turn the user data into an array
-    let bldgArray = new Array();
-    bldgArray.push(bldg1);
-    bldgArray.push(bldg2);
-    bldgArray.push(bldg3);
-    bldgArray.push(bldg4);
-    bldgArray.push(bldg5);
+    
+        let bldgArray = new Array();
+        bldgArray.push(bldg1);
+        bldgArray.push(bldg2);
+        bldgArray.push(bldg3);
+        bldgArray.push(bldg4);
+        bldgArray.push(bldg5);
 
-    // step 3: determine which buildings can see the sunset(create a new array who's job it is to record ascending numbers)
-    // by default the first number will be the max number
-    let maxHeight = bldgArray[0];
-    let acendingArray = new Array();
-    acendingArray.push(maxHeight);
+  
+        let maxHeight = bldgArray[0];
+        let acendingArray = new Array();
+        acendingArray.push(maxHeight);
 
-    // Step 4: cycle through the array and always add the largest numbers
-    // if and only if the current building(number) is larger than the largest previous building(number) will i add it to the viewers(ascending) array
-    for(let loop=1; loop < length; loop++){
-        if(bldgArray[loop] > maxHeight){
-            maxHeight = bldgArray[loop]
-            acendingArray.push(maxHeight);
+    
+        for(let loop=1; loop < bldgArray.length; loop++){
+            if(bldgArray[loop] > maxHeight){
+                maxHeight = bldgArray[loop]
+                acendingArray.push(maxHeight);
         }
          
 
-    }   // Output to id="output" innerText or HTML
-        // step 5 output results to the user
-        document.getElementById("output").innerText = `The Acending array is [${viewers.join(', ')}]`;
+        }  
+        
+        if(acendingArray.length == 1){
+            document.getElementById("output").innerText = `Their is 1 building that can see the Sunset:  [${acendingArray.join(', ')}]`;
+        }else
+        {
+            document.getElementById("output").innerText = `Their are ${acendingArray.length} buildings that can see the Sunsut:  [${acendingArray.join(', ')}]`;
+        }
         
     }
  
+    function Clear(){
+        document.getElementById("input1").value = "";
+        document.getElementById("input2").value = "";
+        document.getElementById("input3").value = "";
+        document.getElementById("input4").value = "";
+        document.getElementById("input5").value = "";
+        document.getElementById("input6").value = "";
+        document.getElementById("output").innerText = "";
+    
 
+    }
     
 
 
 
-function Clear(){
-    document.getElementById("input1").innerText = "";
-}
+    
